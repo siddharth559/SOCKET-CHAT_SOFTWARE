6899e999952413d58d5be21c93f644986b901bd7
Siddharth Acharya <siddharth.acharya02@gmail.com>
2022-11-22 21:42:53 +0530

    Add files via upload

--- sockets.py
+++ sockets.py
@@ -2,11 +2,12 @@ from email.headerregistry import Address
 import socket as sk
 import _thread
 import threading as th
+import os
 
 A = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
 PORT = 52345
 
-HOST = "192.168.0.103"#"103.197.36.6"#for connecting in local "192.168.1.110"  "" for all connection
+HOST = ""#"103.197.36.6"#for connecting in local "192.168.1.110"  "" for all connection
 A.bind((HOST,PORT))
 A.listen(5)
 
@@ -24,15 +25,29 @@ def IO(CONNECTION,index):
     while 1:
         data = CONNECTION.recv(1024)
 
-        try: print(name,":",data.decode())
-        except: print("some error")
+        try:
+            given_file = eval(data)
+            file = open(given_file['file'],'wb')
+            byte_data_recieved = CONNECTION.recv(4096)
+            while byte_data_recieved:
+                file.write(byte_data_recieved)
+                byte_data_recieved = CONNECTION.recv(4096)
+            file.close()
+            print(os.path.getsize(file.name) == given_file['size'])
+            
+            for i in connections:
+                i[0].sendall(data)
 
-        if not data:
-            CONNECTION.sendall(''.encode())
-            #LOCK.release()
-            break
-        for i in connections:
-            i[0].sendall((name+': ').encode()+data)
+        except:
+            try: print(name,":",data.decode())
+            except: print("some error")
+
+            if not data:
+                CONNECTION.sendall(''.encode())
+                #LOCK.release()
+                break
+            for i in connections:
+                i[0].sendall((name+': ').encode()+data)
     io_threads.pop(index)
     connections.pop(index)
 
